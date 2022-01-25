fe3a1819772e20227516c05558adc5a9036ce67e
Max <fix99@mail.ru>
2022-01-25 15:49:25 +0300

    added snake game item

--- scripts/follower.js
+++ scripts/follower.js
@@ -1,7 +1,7 @@
 const el = $(".card");
-$("#top").on("mousemove", function (target) {
-    const rotY = -($(window).innerWidth() / 2 - target.pageX) / 8,
-        rotX = ($(window).innerHeight() / 2 - target.pageY) / 10;
+$("#top").on("mousemove", function (e) {
+    const rotY = -($(window).innerWidth() / 2 - e.pageX) / 8,
+        rotX = ($(window).innerHeight() / 2 - e.pageY) / 8  - 20;
     el.attr("style", "transform: rotateY(" + rotY + "deg) rotateX(" + rotX + "deg);-webkit-transform: rotateY(" + rotY + "deg) rotateX(" + rotX + "deg);-moz-transform: rotateY(" + rotY + "deg) rotateX(" + rotX + "deg)");
 });
 
@@ -9,14 +9,14 @@ $("#top").on("mouseleave", function () {
   el.attr("style", "transform: rotateY(" + 20 + "deg) rotateX(" + 10 + "deg);");
 });
 
-$("#fish").on("click", function (target) {
+$("#fish").on("click", function () {
     $(".frame").css("cursor", "url(../images/fish24s.png), auto");
 });
 
-$("#mouse").on("click", function (target) {
+$("#mouse").on("click", function () {
     $(".frame").css("cursor", "url(../images/mouse24s.png), auto");
 });
 
-$("#reset").on("click", function (target) {
+$("#reset").on("click", function () {
     $(".frame").css("cursor", "default");
 });
