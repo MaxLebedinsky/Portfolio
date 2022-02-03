bda290d22eef0590c4c4808da47c27f9669fe47e
Max <fix99@mail.ru>
2022-02-03 22:40:24 +0300

    added creating new boards

--- scripts/task-board.js
+++ scripts/task-board.js
@@ -1,48 +1,81 @@
-  const lists = document.querySelectorAll('.list');
+const lists = document.querySelectorAll('.list');
+const addBoardButton = document.querySelector('.button');
 
-  function addTask() {
-      const btn = document.querySelector('.add-btn');
-      const addBtn = document.querySelector('.add-item-btn');
-      const cancelBtn = document.querySelector('.cancel-item-btn');
-      const textarea = document.querySelector('.textarea');
-      const form = document.querySelector('.form');
+function addTask() {
+    const btn = document.querySelector('.add-btn');
+    const addBtn = document.querySelector('.add-item-btn');
+    const cancelBtn = document.querySelector('.cancel-item-btn');
+    const textarea = document.querySelector('.textarea');
+    const form = document.querySelector('.form');
 
-      let value;
+    let value;
 
-      btn.addEventListener('click', () => {
-          form.style.display = 'block';
-          btn.style.display = 'none';
-          addBtn.style.display = 'none';
+    btn.addEventListener('click', () => {
+        form.style.display = 'block';
+        btn.style.display = 'none';
+        addBtn.style.display = 'none';
 
-          textarea.addEventListener('input', e => {
-              value = e.target.value;
-              if (value) {
-                  addBtn.style.display = 'block';
-              } else {
-                addBtn.style.display = 'none';
-              }
-          });
-      });
+        textarea.addEventListener('input', e => {
+            value = e.target.value;
+            if (value) {
+                addBtn.style.display = 'block';
+            } else {
+            addBtn.style.display = 'none';
+            }
+        });
+    });
 
-      cancelBtn.addEventListener('click', () => {
-          textarea.value = '';
-          value = '';
-          form.style.display = 'none';
-          btn.style.display = 'flex';
-      });
+    cancelBtn.addEventListener('click', () => {
+    clearForm()
+    });
 
-      addBtn.addEventListener('click', () => {
-            const newItem = document.createElement('div');
-            newItem.classList.add('list-item');
-            newItem.draggable = 'true';
-            newItem.textContent = value;
-            lists[0].append(newItem);
+    addBtn.addEventListener('click', () => {
+        const newItem = document.createElement('div');
+        newItem.classList.add('list-item');
+        newItem.draggable = 'true';
+        newItem.textContent = value;
+        lists[0].append(newItem);
 
-            textarea.value = '';
-            value = '';
-            form.style.display = 'none';
-            btn.style.display = 'flex';
-      });
-  }
+        clearForm()
+    });
 
-  addTask();
+    function clearForm() {
+    textarea.value = '';
+    value = '';
+    form.style.display = 'none';
+    btn.style.display = 'flex';
+    }
+}
+
+addTask();
+
+function addBoard() {
+    const boards = document.querySelector('.boards');
+    const board = document.createElement('div');
+    board.classList.add('boards-item');
+    board.innerHTML = `
+        <span contenteditable="true" class="title">Enter title</span>
+        <div class="list"></div>
+    `;
+    boards.append(board);
+
+    changeTitle();
+}
+
+addBoardButton.addEventListener('click', addBoard);
+
+function changeTitle() {
+    const titles = document.querySelectorAll('.title');
+
+    titles.forEach(title => {
+        title.addEventListener('click', e => e.target.textContent = '');
+    })
+}
+
+changeTitle();
+
+function dragNdrop() {
+
+}
+
+dragNdrop();
