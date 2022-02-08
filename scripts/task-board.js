745ab8ca9ee575b74041e99cc387278f27f739bd
Max <fix99@mail.ru>
2022-02-08 18:02:13 +0300

    added deleting boards

--- scripts/task-board.js
+++ scripts/task-board.js
@@ -1,5 +1,6 @@
 const lists = document.querySelectorAll('.list');
 const addBoardButton = document.querySelector('.button');
+document.querySelectorAll('.list-item').forEach(card => initCloseHandler(card, '.close-card-btn'));
 
 function addTask() {
     const btn = document.querySelector('.add-btn');
@@ -34,20 +35,32 @@ function addTask() {
         newItem.classList.add('list-item');
         newItem.draggable = 'true';
         newItem.textContent = value;
+        newItem.insertAdjacentHTML("beforeend", '<i class="fas fa-times close-card-btn"></i>');
         lists[0].append(newItem);
 
+        initCloseHandler(newItem, '.close-card-btn');
+
         clearForm()
+
+        dragNdrop();
     });
 
     function clearForm() {
-    textarea.value = '';
-    value = '';
-    form.style.display = 'none';
-    btn.style.display = 'flex';
+        textarea.value = '';
+        value = '';
+        form.style.display = 'none';
+        btn.style.display = 'flex';
     }
 }
 
 addTask();
+
+function initCloseHandler (item, selector) {
+    const closeBtn = item.querySelector(selector);
+    closeBtn.addEventListener('click', (e) => {
+        item.remove();
+    })
+}
 
 function addBoard() {
     const boards = document.querySelector('.boards');
@@ -56,26 +69,94 @@ function addBoard() {
     board.innerHTML = `
         <span contenteditable="true" class="title">Enter title</span>
         <div class="list"></div>
+        <i class="far fa-window-close close-board-btn"></i>
     `;
     boards.append(board);
 
-    changeTitle();
+    initChangeTitles();
+
+    dragNdrop();
+
+    initDelBoards();
 }
 
 addBoardButton.addEventListener('click', addBoard);
 
-function changeTitle() {
+function initChangeTitles() {
     const titles = document.querySelectorAll('.title');
 
     titles.forEach(title => {
-        title.addEventListener('click', e => e.target.textContent = '');
+        title.addEventListener('click', e => {
+            if (e.target.textContent === 'Enter title') 
+            {e.target.textContent = ''}
+        });
     })
 }
 
-changeTitle();
+initChangeTitles();
+
+let draggedItem = null; 
 
 function dragNdrop() {
+    const lists = document.querySelectorAll('.list');
+    const listItems = document.querySelectorAll('.list-item');
 
+    for(let i = 0; i < listItems.length; i++) {
+        const item = listItems[i];
+
+        item.addEventListener('dragstart', () => {
+            draggedItem = item;
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 0);
+        });
+
+        item.addEventListener('dragend', () => {
+            setTimeout(() => {
+                item.style.display = 'block';
+                draggedItem = null;
+            }, 0);
+        });
+
+        item.addEventListener('dblclick', (e) => {
+            e.stopPropagation();
+            item.remove();
+        });
+
+        for (let j = 0; j < lists.length; j++) {
+            const list = lists[j];
+
+            list.addEventListener('dragover', (e) => e.preventDefault());
+
+            list.addEventListener('dragenter', function (e) {
+                e.preventDefault();
+                this.style.backgroundColor = 'rgba(0, 0, 0, .3)';
+            });
+
+            list.addEventListener('dragleave', function (e) {
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+            });
+
+            list.addEventListener('drop', function (e) {
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+                this.append(draggedItem);
+            })
+        }
+    }
 }
 
 dragNdrop();
+
+function initDelBoards() {
+    const closeBoardButtons = document.querySelectorAll('.close-board-btn');
+
+    for (let i = 0; i < closeBoardButtons.length; i++) {
+        const cross = closeBoardButtons[i];
+        cross.addEventListener('click', (e) => {
+            let delConfirmed = confirm('Delete this board?');
+            if(delConfirmed) {e.target.parentNode.remove();}
+        });
+    }
+}
+
+initDelBoards()
