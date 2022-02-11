80bd7d253abab24d09038a20ee76da603d8d6f01
Max <fix99@mail.ru>
2022-02-11 08:24:01 +0300

    added expanding panels item

---
+++ scripts/expanding-panels.js
@@ -0,0 +1,20 @@
+function slidesPlugin(activeSlideNumber = 3) {
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlideNumber].classList.add('active');
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
+}
+
+slidesPlugin();
