af653b08340bffddde20044871f62c665b1a069b
Max <fix99@mail.ru>
2022-02-13 23:40:08 +0300

    added buttons; disabled mouse move reactions

--- scripts/bouncing-ball.js
+++ scripts/bouncing-ball.js
@@ -1,8 +1,15 @@
 const frame = document.querySelector('.frame');
 const scene = document.querySelector('.scene');
+const cube = document.querySelector('.cube');
 const ball = document.querySelector('.ball');
+const ballShadow = document.querySelector('.ball-shadow');
+const stopBtn = document.getElementById('stop');
+const startBtn = document.getElementById('start');
+const stopRotBtn = document.getElementById('stop-rotation');
+const startRotBtn = document.getElementById('start-rotation');
 // console.log(window.getComputedStyle(frame));
 let mousePressed = false;
+let rotationPaused = false;
 let timer;
 let rotX = 0;
 let rotY = 0;
@@ -13,31 +20,35 @@ const rotSpeed = .5;
 
 frame.onmousedown = (e) => {
     e.preventDefault();
-    mousePressed = true;
-    console.log('click!', rotX, rotY);
-    clearInterval(timer);
-    console.log('timer cleared', rotX, rotY);
+    e.stopPropagation();
+    console.log('onFrameMouseDOWN');
 }
 
 document.onmouseup = (e) => {
     e.preventDefault();
-    mousePressed = false;
-    startRotateScene();
+    e.stopPropagation();
+    console.log('onDocMouseUP');
 }
 
 frame.onmouseleave = (e) => {
     e.preventDefault();
-    mousePressed = false;
+    e.stopPropagation();
+    console.log('onFrameLEAVE');
+
+}
+
+const normalizeRotX = () => {
     rotX = rotX - (rotX % 360);
+    renderScene(rotX, rotY);
     frame.style.fontSize = `75px`;
-    // rotX = rotX % 360;
-    // rotY = rotY % 360;
-    // renderScene(rotX, rotY);
 }
 
 frame.onmousemove = (e) => {
     e.preventDefault();
+    
     if (mousePressed) {
+
+        console.log('onmouseMove & mousePressed');
 
         if (e.movementY < 0) {
             deltaRotX = -1;
@@ -50,7 +61,6 @@ frame.onmousemove = (e) => {
         rotY = rotY + deltaRotY;
 
         renderScene(rotX, rotY);
-        console.log(rotX, rotY);
 
         deltaRotX = 0;
         deltaRotY = 0;
@@ -65,6 +75,57 @@ frame.onwheel = (e) => {
     frame.style.fontSize = `${75 + zoom}px`
 }
 
+stopBtn.onmousedown = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('stopBounceBtn');
+    ball.style.animationPlayState = 'paused';
+    ballShadow.style.animationPlayState = 'paused';
+    cube.style.animationPlayState = 'paused';
+    startBtn.classList.remove('disabled');
+    stopBtn.classList.add('disabled');
+}
+
+startBtn.onmousedown = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('startBounceBtn');
+    ball.style.animationPlayState = 'running';
+    ballShadow.style.animationPlayState = 'running';
+    cube.style.animationPlayState = 'running';
+    startBtn.classList.add('disabled');
+    stopBtn.classList.remove('disabled');
+}
+
+stopRotBtn.onmousedown = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('stopRotBtn');
+    if (!rotationPaused) {
+        clearInterval(timer);
+    }
+    rotationPaused = true;
+    startRotBtn.classList.remove('disabled');
+    stopRotBtn.classList.add('disabled');
+}
+
+startRotBtn.onmousedown = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('startRotBtn');
+    if (rotationPaused) {
+        startRotateScene();
+    }
+    rotationPaused = false;
+    startRotBtn.classList.add('disabled');
+    stopRotBtn.classList.remove('disabled');
+}
+
+startBtn.onmouseup = (e) => { e.stopPropagation() }
+stopBtn.onmouseup = (e) => { e.stopPropagation() }
+startRotBtn.onmouseup = (e) => { e.stopPropagation() }
+stopRotBtn.onmouseup = (e) => { e.stopPropagation() }
+
 const renderScene = (rotX = 0, rotY = 0) => {
     scene.style.transform = `rotateY(${rotY}deg) rotateX(${-rotX}deg)`;
     ball.style.transform = `rotateY(${-rotY}deg) rotateX(${rotX*Math.cos(rotY * Math.PI / 180)}deg)`;
@@ -73,9 +134,7 @@ const renderScene = (rotX = 0, rotY = 0) => {
 const startRotateScene = () => {
     timer = setInterval(() => {
         renderScene(rotX, rotY);
-        // rotY = rotY >= 360 ? rotY % 360 : rotY + 1;
         rotY = rotY + rotSpeed;
-        console.log(rotX, rotY);
     }, 30);
 }
 
